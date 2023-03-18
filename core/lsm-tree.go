94e84a3003e90e8db117ddee7eaa09afff89ec62
jokingmoony <penubokim@gmail.com>
2023-03-18 12:45:40 +0900

    Flush buffer if it reaches its capacity

--- core/lsm-tree.go
+++ core/lsm-tree.go
@@ -6,16 +6,21 @@ import (
 
 type LSMTree struct {
 	memBuffer map[int]string
+	capacity  int
 }
 
 func NewLSMTree() *LSMTree {
 	tree := &LSMTree{memBuffer: make(map[int]string)}
+	tree.capacity = 10
 	return tree
 }
 
 func (tree *LSMTree) Insert(key int, value string) {
+	if len(tree.memBuffer) >= tree.capacity {
+		tree.flushToDisk()
+	}
 	tree.memBuffer[key] = value
-	fmt.Println("Hello world", tree.memBuffer)
+	fmt.Println(tree.memBuffer)
 }
 
 func (tree *LSMTree) Find(key int) {
@@ -24,4 +29,9 @@ func (tree *LSMTree) Find(key int) {
 
 func (tree *LSMTree) Delete(key int) {
 	fmt.Println("Can't delete", key)
+}
+
+func (tree *LSMTree) flushToDisk() {
+	// TODO: flush to the disk
+	tree.memBuffer = make(map[int]string)
 }
